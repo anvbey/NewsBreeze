b443ba4843e9f721b4978ec679d30cd82583b188
Anvit_Dubey <2005220@kiit.ac.in>
2022-10-09 13:04:30 +0530

    First commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/api/RetrofitInstance.kt
@@ -0,0 +1,66 @@
+package com.androiddevs.mvvmnewsapp.api
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import com.androiddevs.mvvmnewsapp.util.Constants.Companion.BASE_URL
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitInstance(context: Context) {
+
+
+        private fun hasNetwork(context: Context): Boolean? {
+            var isConnected: Boolean? = false // Initial Value
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+            if (activeNetwork != null && activeNetwork.isConnected)
+                isConnected = true
+            return isConnected
+        }
+
+        private val retrofit by lazy {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .cache(Cache(context.cacheDir,5*1024*1024))
+                .addInterceptor { chain ->
+                    var request = chain.request()
+                    request = if (hasNetwork(context)!!)
+                        request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
+                    else
+                        request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7).build()
+                    chain.proceed(request)
+                }
+                .addInterceptor(logging)
+                .build()
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+        }
+        val api  = retrofit.create(NewsAPI::class.java)
+    companion object {
+
+        private val retrofit by lazy {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+        }
+
+        val api by lazy {
+            retrofit.create(NewsAPI::class.java)
+        }
+    }
+}
